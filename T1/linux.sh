efcf058dab9662b676acf78f79127f66db8760dd
sleepy_wolf <43232088+ceccs17d40@users.noreply.github.com>
2019-05-13 02:33:53 +0530

    Add files via upload

---
+++ T1/linux.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+ls
+ls -lk
+ls -1
+ls -ltr
+alias ll='ls -l'
+ll
+unalias ll
+ll
+pwd
+cat > a.txt
+cat a.txt > b.txt 
+grep "new" aa.txt 
+cd Desktop/
+ls
+mkdir foss
+ls
+rmdir foss
+ls
+rm -r foss
+ls
+cat > c.txt
+cat > d.txt
+join c.txt d.txt 
+exit
